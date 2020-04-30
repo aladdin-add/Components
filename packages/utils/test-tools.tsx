a1ca60260c4439db5705c9f9bc591a233b0e0f0b
The-Code-Monkey <31218527+The-Code-Monkey@users.noreply.github.com>
2020-04-30 11:07:14 +0100

    Create test-tools.tsx

---
+++ packages/utils/test-tools.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { mount, shallow } from "enzyme";
+
+
+import { ThemeProvider } from "@orchard/theme/theme-provider";
+import { ThemeModeEnum } from "@orchard/theme/enums/themeModeEnum";
+
+import theme from "@/theme";
+
+const removeProperties = () => {
+  const keys = ["theme", "styledTheme"];
+
+  return {
+    test: val => {
+      return (
+        val &&
+        typeof val === "object" &&
+        "props" in val &&
+        Object.keys(val.props).some(prop => keys.some(key => key === prop))
+      );
+    },
+    print: (val, serialize) => {
+      keys.forEach(key => {
+        delete val.props[key];
+      });
+      return serialize(val);
+    },
+  };
+};
+
+export const shallowWithTheme = children => 
+  shallow(<ThemeProvider theme={theme} mode={ThemeModeEnum.DARK}>{children}</ThemeProvider>)
+    .dive()
+    .shallow();
+    
+export const mountWithTheme = children => {
+  expect.addSnapshotSerializer(removeProperties());
+  
+  return (mount(<ThemeProvider theme={theme} mode={ThemeModeEnum.DARK}>{children}</ThemeProvider>);
+};
