aec4bf2cfc9a0f3ee1028d0548466c8b1dc8b944
The-Code-Monkey <31218527+The-Code-Monkey@users.noreply.github.com>
2020-05-05 09:15:25 +0100

    Update and rename packages/primatives/box/Box.tsx to packages/primatives/view/View.tsx

--- packages/primatives/box/Box.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import StyledBox from "./styled";
-
-const Box = ({ children }) => <StyledBox>{children}</StyledBox>;
-
-export default Box;
---
+++ packages/primatives/view/View.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import StyledBox from "./styled";
+
+const View = ({ children }) => <StyledView>{children}</StyledView>;
+
+export default View;
