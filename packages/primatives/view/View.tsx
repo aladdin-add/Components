ac31e2793b3979c04f5ce23821fd86b5489d4e74
The-Code-Monkey <31218527+The-Code-Monkey@users.noreply.github.com>
2021-03-09 19:56:55 +0000

    Update view

--- packages/primatives/view/View.tsx
+++ packages/primatives/view/View.tsx
@@ -1,8 +1,25 @@
 import React, { ReactNode } from "react";
+import { 
+  SpaceProps, 
+  ColorProps, 
+  LayoutProps, 
+  PositionProps, 
+  BorderProps,
+  ShadowProps,
+  FlexboxProps
+} from "styled-system";
 
 import { StyledView } from "./styled";
 
-export interface Props {
+export interface Props extends 
+  SpaceProps, 
+  ColorProps, 
+  LayoutProps, 
+  PositionProps,
+  BorderProps,
+  ShadowProps,
+  FlexboxProps
+{
   children: ReactNode;
   as?: any;
   autoid: string;
