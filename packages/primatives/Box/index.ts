73a0d5f69e58b152ba86e7ae4a20cb6e6091cb89
The-Code-Monkey <31218527+The-Code-Monkey@users.noreply.github.com>
2021-03-10 07:41:52 +0000

    Update index.ts

--- packages/primatives/Box/index.ts
+++ packages/primatives/Box/index.ts
@@ -1,4 +1,5 @@
 import Box, { Props as BoxProps } from "./Box";
+import { StyledBox, BoxStyledProps } from "./styled";
 
-export { BoxProps };
+export { BoxProps, StyledBox, BoxStyledProps };
 export default Box
