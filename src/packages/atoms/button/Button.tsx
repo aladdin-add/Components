013fb421664f0f6b01a1d8ec2b265464363831c6
The-Code-Monkey <andy.wilson1234@gmail.com>
2021-05-20 10:04:55 +0000

    fix: bit more conversion

--- src/packages/atoms/button/Button.tsx
+++ src/packages/atoms/button/Button.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import { InteractableProps } from "@/primatives/interactable";
 import Text from "@/primatives/text";
 
--- src/packages/primatives/icon/utils.ts
+++ src/packages/primatives/icon/utils.ts
@@ -3,7 +3,6 @@ import lazy from "react-lazy-named";
 export const getIcon = (config, name) => {
   if (config.iconDir) {
     switch (config.iconDir) {
-      case "feather":
       default:
         return lazy(() => import("react-feather"), name);
     }
