23b70f8fc5ee08191ab3551a277dd112ce4508c7
The-Code-Monkey <andy.wilson1234@gmail.com>
2021-06-30 11:07:05 +0000

    feat: add grid and cell

---
+++ src/packages/atoms/grid/utils.ts
@@ -0,0 +1,21 @@
+import { GridProps } from '@aw-web-design/styled-system';
+
+export const getTemplate = (
+  template: GridProps['gridTemplateColumns'] | GridProps['gridTemplateRows']
+) => {
+  if (Array.isArray(template)) {
+    return template.map((value) =>
+      typeof value === 'number' ? `repeat(${template},1fr)` : value
+    );
+  }
+  return typeof template === 'number' ? `repeat(${template},1fr)` : template;
+};
+
+export const getGrid = (edge: number, value: number): string | number => {
+  if (edge && value) {
+    return `${edge} / span ${value}`;
+  } else if (value) {
+    return `span ${value}`;
+  }
+  return edge;
+};
