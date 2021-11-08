b590fdbec7c0d7cf675aa7564925744a43630977
The-Code-Monkey <31218527+The-Code-Monkey@users.noreply.github.com>
2021-11-08 15:38:49 +0000

    Update setupTests.js

--- setupTests.js
+++ setupTests.js
@@ -1,28 +1,4 @@
 const Enzyme = require('enzyme');
 const Adapter = require('@wojtekmaj/enzyme-adapter-react-17');
-const { JSDOM } = require('jsdom');
-
-const jsdom = new JSDOM('<!doctype html><html><body></body></html>', { url: "http://localhost" });
-const { window } = jsdom;
-
-function copyProps(src, target) {
-    Object.defineProperties(target, {
-      ...Object.getOwnPropertyDescriptors(src),
-      ...Object.getOwnPropertyDescriptors(target),
-    });
-  }
-  
-  global.window = window;
-  global.document = window.document;
-  global.navigator = {
-    userAgent: 'node.js',
-  };
-  global.requestAnimationFrame = function (callback) {
-    return setTimeout(callback, 0);
-  };
-  global.cancelAnimationFrame = function (id) {
-    clearTimeout(id);
-  };
-  copyProps(window, global);
 
 Enzyme.configure({ adapter: new Adapter() });
